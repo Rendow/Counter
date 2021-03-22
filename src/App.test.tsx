882c629b333bca237604b0d35248ca66d71e00e4
Rendow <rendow33@gmail.com>
2021-03-22 19:18:48 +0300

    second commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './CounterDisplay/Display';
+import App from './Counter/CounterDisplay/Display';
 
 test('renders learn react link', () => {
   render(<App />);
